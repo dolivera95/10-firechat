94b290a1db384f0cbc7a785925370b84ec16928d
Daniel Olivera <dolivera95@gmail.com>
2019-03-30 11:09:55 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+//import { AngularFirestore } from '@angular/fire/firestore';
+//import { Observable } from 'rxjs';
+import {ChatService} from './providers/chat.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'firechat';
+//  public chats: Observable<any[]>;
+
+  constructor(/*db: AngularFirestore*/ public _cs:ChatService) {
+//    this.chats = db.collection('chats').valueChanges();
+  }
+}
